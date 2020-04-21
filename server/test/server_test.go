68c6f7dff56e06fe2ce4a7c36c886ace8a7cd741
Chris Riley <rileyc@gmail.com>
2020-04-21 13:53:19 -0700

    add server test for Move command

--- server/test/server_test.go
+++ server/test/server_test.go
@@ -16,6 +16,8 @@ import (
 
 const ServerTestPort = 8080
 
+type commandParams map[string]interface{}
+
 type gameState struct {
 	NextMove   string       `json:"next_move"`
 	RedScore   int          `json:"red_score"`
@@ -28,16 +30,58 @@ type newGameResponse struct {
 	State gameState `json:"state"`
 }
 
+type moveResponse struct {
+	MoveStatus string    `json:"move_status"`
+	State      gameState `json:"state"`
+}
+
+func post(url string, command string, params commandParams) (responseStatusCode int, responseBodyBytes []byte, err error) {
+	requestBodyBytes, marshalErr := json.Marshal(commandParams{
+		"command": command,
+		"params":  params,
+	})
+	if marshalErr != nil {
+		log.Fatalln(marshalErr)
+	}
+
+	response, postErr := http.Post(url, "application/json", bytes.NewBuffer(requestBodyBytes))
+	if postErr != nil {
+		err = postErr
+		return
+	}
+
+	var readErr error
+	responseBodyBytes, readErr = ioutil.ReadAll(response.Body)
+	if readErr != nil {
+		err = readErr
+		return
+	}
+
+	responseStatusCode = response.StatusCode
+
+	fmt.Println()
+	fmt.Println("*****************************************************")
+	fmt.Println("POST REQUEST BODY")
+	fmt.Println(string(requestBodyBytes))
+	fmt.Println()
+
+	fmt.Println("POST RESPONSE BODY")
+	fmt.Println(string(responseBodyBytes))
+	fmt.Println()
+
+	return
+}
+
 var _ = Describe("Game Server Tests", func() {
 	var subject server.GameServer
 	var command string
-	var params map[string]string
+	var params commandParams
 	var responseStatusCode int
 
 	url := fmt.Sprintf("http://localhost:%d/api/v1/game", ServerTestPort)
 
 	BeforeEach(func() {
-		params = make(map[string]string)
+		params = make(commandParams)
 		go func() {
 			subject.Start(ServerTestPort)
 		}()
@@ -45,27 +89,11 @@ var _ = Describe("Game Server Tests", func() {
 
 	Describe("POST operations", func() {
 		var bodyBytes []byte
+		var err error
 
 		JustBeforeEach(func() {
-			requestBody, marshalErr := json.Marshal(map[string]interface{}{
-				"command": command,
-				"params":  params,
-			})
-			if marshalErr != nil {
-				log.Fatalln(marshalErr)
-			}
-
-			response, postErr := http.Post(url, "application/json", bytes.NewBuffer(requestBody))
-			if postErr != nil {
-				log.Fatalln(postErr)
-			}
-
-			responseStatusCode = response.StatusCode
-
-			var readErr error
-			bodyBytes, readErr = ioutil.ReadAll(response.Body)
-			if readErr != nil {
-				log.Fatal(readErr)
+			if responseStatusCode, bodyBytes, err = post(url, command, params); err != nil {
+				log.Fatalln(err)
 			}
 		})
 
@@ -75,7 +103,7 @@ var _ = Describe("Game Server Tests", func() {
 			JustBeforeEach(func() {
 				unmarshalErr := json.Unmarshal(bodyBytes, &response)
 				if unmarshalErr != nil {
-					log.Fatal(unmarshalErr)
+					log.Fatalln(unmarshalErr)
 				}
 			})
 
@@ -101,6 +129,91 @@ var _ = Describe("Game Server Tests", func() {
 				It("reports an empty game board", func() {
 					Expect(response.State.BoardState.Peg[0]).To(Equal(engine.Peg{
 						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+					Expect(response.State.BoardState.Peg[1]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+					Expect(response.State.BoardState.Peg[2]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+					Expect(response.State.BoardState.Peg[3]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+					Expect(response.State.BoardState.Peg[4]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+					Expect(response.State.BoardState.Peg[5]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+					Expect(response.State.BoardState.Peg[6]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+					Expect(response.State.BoardState.Peg[7]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
+					}))
+				})
+			})
+		})
+
+		Describe("Move command", func() {
+			var response moveResponse
+
+			BeforeEach(func() {
+				command = "move"
+			})
+
+			JustBeforeEach(func() {
+				unmarshalErr := json.Unmarshal(bodyBytes, &response)
+				if unmarshalErr != nil {
+					log.Fatalln(unmarshalErr)
+				}
+			})
+
+			Describe("with new game, player to move", func() {
+				var gameId string
+
+				BeforeEach(func() {
+					newGameParams := commandParams{}
+					newGameParams["colour"] = "red"
+					newGameParams["move_first"] = "TRUE"
+					statusCode, bodyBytes, err := post(url, "newgame_1p", newGameParams)
+					if err != nil {
+						log.Fatalln(err)
+					}
+					if statusCode != 200 {
+						log.Fatalln("failed to create new game")
+					}
+
+					var response newGameResponse
+					unmarshalErr := json.Unmarshal(bodyBytes, &response)
+					if unmarshalErr != nil {
+						log.Fatalln(unmarshalErr)
+					}
+
+					gameId = response.Id
+				})
+
+				BeforeEach(func() {
+					params["id"] = gameId
+					params["peg"] = "A"
+				})
+
+				It("succeeds", func() {
+					Expect(responseStatusCode).To(Equal(200))
+				})
+				It("accepts the move", func() {
+					Expect(response.MoveStatus).To(Equal("accepted"))
+				})
+				It("expects Blue to move next", func() {
+					Expect(response.State.NextMove).To(Equal("BlueToMove"))
+				})
+				It("reports a score of 0-0", func() {
+					Expect(response.State.RedScore).To(BeZero())
+					Expect(response.State.BlueScore).To(BeZero())
+				})
+				It("reports a game board with one red peg", func() {
+					Expect(response.State.BoardState.Peg[0]).To(Equal(engine.Peg{
+						Slot: [3]engine.Colour{engine.Red, engine.None, engine.None},
 					}))
 					Expect(response.State.BoardState.Peg[1]).To(Equal(engine.Peg{
 						Slot: [3]engine.Colour{engine.None, engine.None, engine.None},
