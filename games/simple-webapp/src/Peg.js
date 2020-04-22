61aafa24c7557ab0d174bc8efefbfd1b2b416d33
Chris Riley <rileyc@gmail.com>
2020-04-22 15:24:26 -0700

    try removing HTML tables

--- games/simple-webapp/src/Peg.js
+++ games/simple-webapp/src/Peg.js
@@ -6,36 +6,20 @@ class Peg extends Component {
     render() {
         if (this.props.value === undefined) {
             return (
-                <table style={{display: "inline-block"}}>
-                    <tbody>
-                        <tr>
-                            <td><Slot/></td>
-                        </tr>
-                        <tr>
-                            <td><Slot/></td>
-                        </tr>
-                        <tr>
-                            <td><Slot/></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div style={{display: "inline-block"}}>
+                    <div><Slot/></div>
+                    <div><Slot/></div>
+                    <div><Slot/></div>
+                </div>
             )
         }
         else {
             return (
-                <table style={{display: "inline-block"}}>
-                    <tbody>
-                        <tr>
-                            <td><Slot value={this.props.value.Slot[2]}/></td>
-                        </tr>
-                        <tr>
-                            <td><Slot value={this.props.value.Slot[1]}/></td>
-                        </tr>
-                        <tr>
-                            <td><Slot value={this.props.value.Slot[0]}/></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div style={{display: "inline-block"}}>
+                    <div><Slot value={this.props.value.Slot[2]}/></div>
+                    <div><Slot value={this.props.value.Slot[1]}/></div>
+                    <div><Slot value={this.props.value.Slot[0]}/></div>
+                </div>
             )
         }
     }
