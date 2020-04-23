16b86ab19d4c4340dd01ba79ea56f92ad523ba4d
Chris Riley <rileyc@gmail.com>
2020-04-23 11:41:13 -0700

    move game state change logic out of App.js

--- games/simple-webapp/src/App.js
+++ games/simple-webapp/src/App.js
@@ -1,154 +1,58 @@
 import React, { Component } from "react"
 import { hot } from "react-hot-loader"
 import "./App.css"
-import { ColourPicker, Colour } from "./ColourPicker.js"
+import { ColourPicker } from "./ColourPicker.js"
 import { MoveFirstPicker } from "./MoveFirstPicker.js"
-import { Game, NextAction, ActionResultStatus } from "./Game.js"
+import { Game } from "./Game.js"
 
-const NextMove = {
-    RED_TO_MOVE: "RedToMove",
-    BLUE_TO_MOVE: "BlueToMove"
+const NextAction = {
+    PICK_COLOUR: 1,
+    PICK_FIRST_MOVE: 2,
+    PLAY_GAME: 3
 }
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            gameId: null,
+            nextAction: NextAction.PICK_COLOUR,
             playerColour: null,
-            nextAction: NextAction.START_NEW_GAME,
-            moveFirst: null,
-            boardState: null,
-            playerLastPegClick: null,
-            redScore: 0,
-            blueScore: 0,
-            error: null,
+            moveFirst: null
         }
     }
 
-
-    renderMoveFirstPicker() {
-        return (
-            <MoveFirstPicker cb={moveFirst => this.handleMoveFirstClick(moveFirst)} />
-        )
-    }
-
-    renderGame() {
-        return (
-            <Game
-                gameId={this.state.gameId}
-                action={this.state.nextAction}
-                colour={this.state.playerColour}
-                redScore={this.state.redScore}
-                blueScore={this.state.blueScore}
-                move_first={this.state.moveFirst}
-                board_state={this.state.boardState}
-                error={this.state.error}
-                player_last_click={this.state.playerLastPegClick}
-                actionResultCb={actionResult => this.handleActionResult(actionResult)}
-                pegClickCb={peg => this.handlePegClick(peg)}
-            />
-        )
-    }
-
-    renderColourPicker() {
-        return (
-            <ColourPicker cb={colour => this.handleColourClick(colour)} />
-        )
-    }
-
     render() {
-        if (this.state.playerColour == null) {
-            return this.renderColourPicker()
-        } else if (this.state.moveFirst == null) {
-            return this.renderMoveFirstPicker()
-        } else {
-            return this.renderGame()
+        if (this.state.nextAction == NextAction.PICK_COLOUR) {
+            return (
+                <ColourPicker cb={colour => this.handleColourClick(colour)} />
+            )
+        }
+        else if (this.state.nextAction == NextAction.PICK_FIRST_MOVE) {
+            return (
+                <MoveFirstPicker cb={moveFirst => this.handleMoveFirstClick(moveFirst)} />
+            )
+        }
+        else {
+            return (
+                <Game playerColour={this.state.playerColour} moveFirst={this.state.moveFirst}/>
+            )
         }
     }
 
     handleColourClick(colour) {
         console.log("colour selected: " + colour)
         this.state.playerColour = colour
+        this.state.nextAction = NextAction.PICK_FIRST_MOVE
+
         this.setState(this.state)
     }
 
     handleMoveFirstClick(moveFirst) {
         console.log("move first: " + moveFirst)
         this.state.moveFirst = (moveFirst == "yes" ? true : false)
-        this.state.nextMoveColour = (this.state.moveFirst ? this.state.playerColour : (this.state.playerColour == Colour.Red ? Colour.Blue : Colour.Red))
+        this.state.nextAction = NextAction.PLAY_GAME
+
         this.setState(this.state)
-    }
-
-    handlePegClick(peg) {
-        console.log("peg clicked: ", peg)
-        if (this.state.nextAction == NextAction.PLAYER_TO_MOVE) {
-            this.state.playerLastPegClick = peg
-            this.state.nextAction = NextAction.PLAYER_MOVING
-            this.setState(this.state)
-        }
-    }
-
-    handleActionResult(actionResult) {
-        if (actionResult.status == ActionResultStatus.SUCCESS) {
-            if (this.state.nextAction == NextAction.START_NEW_GAME) {
-                this.state.gameId = actionResult.id
-                console.log("handleActionResult(): action was: NextAction.START_NEW_GAME, actionResult.NextMove: ", actionResult.nextMove)
-                if (actionResult.nextMove == NextMove.RED_TO_MOVE && this.state.playerColour == Colour.Red ||
-                    actionResult.nextMove == NextMove.BLUE_TO_MOVE && this.state.playerColour == Colour.Blue) {
-                    this.state.nextAction = NextAction.PLAYER_TO_MOVE
-                    console.log("changing state to: NextAction.PLAYER_TO_MOVE")
-                }
-                else if (actionResult.nextMove == NextMove.RED_TO_MOVE && this.state.playerColour == Colour.Blue ||
-                    actionResult.nextMove == NextMove.BLUE_TO_MOVE && this.state.playerColour == Colour.Red) {
-                    this.state.nextAction = NextAction.CPU_TO_MOVE
-                    console.log("changing state to: NextAction.CPU_TO_MOVE")
-                }
-
-                this.state.boardState = actionResult.boardState
-                this.state.redScore = actionResult.redScore
-                this.state.blueScore = actionResult.blueScore
-
-                this.setState(this.state)
-            }
-            else if (this.state.nextAction == NextAction.PLAYER_MOVING) {
-                console.log("got success for PLAYER_MOVING state")
-                this.state.boardState = actionResult.boardState
-                this.state.nextAction = NextAction.CPU_TO_MOVE
-                this.state.redScore = actionResult.redScore
-                this.state.blueScore = actionResult.blueScore
-
-                this.setState(this.state)
-            }
-            else if (this.state.nextAction == NextAction.CPU_TO_MOVE) {
-                console.log("got success for CPU_TO_MOVE state")
-                this.state.boardState = actionResult.boardState
-                this.state.nextAction = NextAction.PLAYER_TO_MOVE
-                this.state.redScore = actionResult.redScore
-                this.state.blueScore = actionResult.blueScore
-
-                this.setState(this.state)
-            }
-        } else {
-            if (this.state.nextAction == NextAction.START_NEW_GAME) {
-                this.state.nextAction = NextAction.START_NEW_GAME_FAILED
-                this.state.error = actionResult.errorMsg
-
-                this.setState(this.state)
-            }
-            else if (this.state.nextAction == NextAction.PLAYER_MOVING) {
-                this.state.nextAction = NextAction.PLAYER_MOVING_FAILED
-                this.state.error = actionResult.errorMsg
-
-                this.setState(this.state)
-            }
-            else if (this.state.nextAction == NextAction.CPU_TO_MOVE) {
-                this.state.nextAction = NextAction.CPU_TO_MOVE_FAILED
-                this.state.error = actionResult.errorMsg
-
-                this.setState(this.state)
-            }
-        }
     }
 }
 
--- games/simple-webapp/src/Game.js
+++ games/simple-webapp/src/Game.js
@@ -16,25 +16,45 @@ const NextAction = {
     CPU_TO_MOVE_FAILED: 7
 }
 
+const NextMove = {
+    RED_TO_MOVE: "RedToMove",
+    BLUE_TO_MOVE: "BlueToMove"
+}
+
 const ActionResultStatus = {
     SUCCESS: "success",
     FAIL: "fail"
 }
 
 class Game extends Component {
-    postCommand(command, params) {
+    constructor(props) {
+        super(props)
+        this.state = {
+            gameId: null,
+            playerColour: props.playerColour,
+            nextAction: NextAction.START_NEW_GAME,
+            moveFirst: props.moveFirst,
+            nextMoveColour: (props.moveFirst ? props.playerColour : (props.playerColour == Colour.Red ? Colour.Blue : Colour.Red)),
+            boardState: null,
+            playerLastPegClick: null,
+            redScore: 0,
+            blueScore: 0,
+            error: null,
+        }
+    }
+
+    postCommand(command, params, cb) {
         var xhttp = new XMLHttpRequest()
         var url = 'http://localhost:8080/api/v1/game'
 
         xhttp.open('POST', url, true)
         xhttp.setRequestHeader('Content-type', 'application/json')
 
-        var that = this
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4) {
                 if (this.status == 200) {
                     var jsonResponse = JSON.parse(xhttp.responseText)
-                    that.props.actionResultCb({
+                    cb({
                         status: ActionResultStatus.SUCCESS,
                         id: jsonResponse.id,
                         nextMove: jsonResponse.state.next_move,
@@ -45,7 +65,7 @@ class Game extends Component {
                 }
                 else {
                     var jsonResponse = JSON.parse(xhttp.responseText)
-                    that.props.actionResultCb({
+                    cb({
                         status: ActionResultStatus.FAIL,
                         errorMsg: jsonResponse.error,
                     })
@@ -60,92 +80,92 @@ class Game extends Component {
         xhttp.send(body)
     }
     render() {
-        if (this.props.action == NextAction.START_NEW_GAME) {
+        if (this.state.nextAction == NextAction.START_NEW_GAME) {
             var params = {
-                "colour": (this.props.colour == Colour.Red ? "red" : "blue"),
-                "move_first": (this.props.move_first ? "TRUE" : "FALSE"),
+                "colour": (this.state.playerColour == Colour.Red ? "red" : "blue"),
+                "move_first": (this.state.moveFirst ? "TRUE" : "FALSE"),
             }
-            this.postCommand("newgame_1p", params)
+            this.postCommand("newgame_1p", params, result => this.handleActionResult(result))
             return (
                 <h1>Start New Game</h1>
             )
         }
-        else if (this.props.action == NextAction.START_NEW_GAME_FAILED) {
+        else if (this.state.nextAction == NextAction.START_NEW_GAME_FAILED) {
             return (
-                <GameError msg={this.props.error}/>
+                <GameError msg={this.state.error}/>
             )
         }
-        else if (this.props.action == NextAction.PLAYER_TO_MOVE) {
+        else if (this.state.nextAction == NextAction.PLAYER_TO_MOVE) {
             return (
                 <div>
                     <h1>Player to move</h1>
                     <Board
-                        value={this.props.board_state}
-                        cb={this.props.pegClickCb}
+                        value={this.state.boardState}
+                        cb={peg => this.handlePegClick(peg)}
                     />
                     <br/>
                     <br/>
                     <Scoreboard
-                        redScore={this.props.redScore}
-                        blueScore={this.props.blueScore}
-                        playerColour={this.props.colour}
+                        redScore={this.state.redScore}
+                        blueScore={this.state.blueScore}
+                        playerColour={this.state.playerColour}
                     />
                 </div>
             )
         }
-        else if (this.props.action == NextAction.PLAYER_MOVING) {
+        else if (this.state.nextAction == NextAction.PLAYER_MOVING) {
             var params = {
-                "id": this.props.gameId,
-                "peg": (this.props.player_last_click),
+                "id": this.state.gameId,
+                "peg": (this.state.playerLastPegClick),
             }
-            this.postCommand("move", params)
+            this.postCommand("move", params, result => this.handleActionResult(result))
             return (
                 <div>
                     <h1>Player moving...</h1>
                     <Board
-                        value={this.props.board_state}
-                        cb={this.props.pegClickCb}
+                        value={this.state.boardState}
+                        cb={this.handlePegClick}
                     />
                     <br/>
                     <br/>
                     <Scoreboard
-                        redScore={this.props.redScore}
-                        blueScore={this.props.blueScore}
-                        playerColour={this.props.colour}
+                        redScore={this.state.redScore}
+                        blueScore={this.state.blueScore}
+                        playerColour={this.state.playerColour}
                     />
                 </div>
             )
         }
-        else if (this.props.action == NextAction.PLAYER_MOVING_FAILED) {
+        else if (this.state.nextAction == NextAction.PLAYER_MOVING_FAILED) {
             return (
-                <GameError msg={this.props.error}/>
+                <GameError msg={this.state.error}/>
             )
         }
-        else if (this.props.action == NextAction.CPU_TO_MOVE) {
+        else if (this.state.nextAction == NextAction.CPU_TO_MOVE) {
             var params = {
-                "id": this.props.gameId,
+                "id": this.state.gameId,
             }
-            this.postCommand("cpu_move", params)
+            this.postCommand("cpu_move", params, result => this.handleActionResult(result))
             return (
                 <div>
                     <h1>CPU to move</h1>
                     <Board
-                        value={this.props.board_state}
-                        cb={this.props.pegClickCb}
+                        value={this.state.boardState}
+                        cb={this.handlePegClick}
                     />
                     <br/>
                     <br/>
                     <Scoreboard
-                        redScore={this.props.redScore}
-                        blueScore={this.props.blueScore}
-                        playerColour={this.props.colour}
+                        redScore={this.state.redScore}
+                        blueScore={this.state.blueScore}
+                        playerColour={this.state.playerColour}
                     />
                 </div>
             )
         }
-        else if (this.props.action == NextAction.CPU_TO_MOVE_FAILED) {
+        else if (this.state.nextAction == NextAction.CPU_TO_MOVE_FAILED) {
             return (
-                <GameError msg={this.props.error}/>
+                <GameError msg={this.state.error}/>
             )
         }
         else {
@@ -154,6 +174,73 @@ class Game extends Component {
             )
         }
     }
+
+    handleActionResult(actionResult) {
+        if (actionResult.status == ActionResultStatus.SUCCESS) {
+            if (this.state.nextAction == NextAction.START_NEW_GAME) {
+                this.state.gameId = actionResult.id
+                this.state.boardState = actionResult.boardState
+                this.state.redScore = actionResult.redScore
+                this.state.blueScore = actionResult.blueScore
+
+                if (actionResult.nextMove == NextMove.RED_TO_MOVE && this.state.playerColour == Colour.Red ||
+                    actionResult.nextMove == NextMove.BLUE_TO_MOVE && this.state.playerColour == Colour.Blue) {
+                    this.state.nextAction = NextAction.PLAYER_TO_MOVE
+                }
+                else if (actionResult.nextMove == NextMove.RED_TO_MOVE && this.state.playerColour == Colour.Blue ||
+                    actionResult.nextMove == NextMove.BLUE_TO_MOVE && this.state.playerColour == Colour.Red) {
+                    this.state.nextAction = NextAction.CPU_TO_MOVE
+                }
+
+                this.setState(this.state)
+            }
+            else if (this.state.nextAction == NextAction.PLAYER_MOVING) {
+                this.state.boardState = actionResult.boardState
+                this.state.nextAction = NextAction.CPU_TO_MOVE
+                this.state.redScore = actionResult.redScore
+                this.state.blueScore = actionResult.blueScore
+
+                this.setState(this.state)
+            }
+            else if (this.state.nextAction == NextAction.CPU_TO_MOVE) {
+                this.state.boardState = actionResult.boardState
+                this.state.nextAction = NextAction.PLAYER_TO_MOVE
+                this.state.redScore = actionResult.redScore
+                this.state.blueScore = actionResult.blueScore
+
+                this.setState(this.state)
+            }
+        } else {
+            if (this.state.nextAction == NextAction.START_NEW_GAME) {
+                this.state.nextAction = NextAction.START_NEW_GAME_FAILED
+                this.state.error = actionResult.errorMsg
+
+                this.setState(this.state)
+            }
+            else if (this.state.nextAction == NextAction.PLAYER_MOVING) {
+                this.state.nextAction = NextAction.PLAYER_MOVING_FAILED
+                this.state.error = actionResult.errorMsg
+
+                this.setState(this.state)
+            }
+            else if (this.state.nextAction == NextAction.CPU_TO_MOVE) {
+                this.state.nextAction = NextAction.CPU_TO_MOVE_FAILED
+                this.state.error = actionResult.errorMsg
+
+                this.setState(this.state)
+            }
+        }
+    }
+
+    handlePegClick(peg) {
+        console.log("peg clicked: ", peg)
+        if (this.state.nextAction == NextAction.PLAYER_TO_MOVE) {
+            this.state.playerLastPegClick = peg
+            this.state.nextAction = NextAction.PLAYER_MOVING
+
+            this.setState(this.state)
+        }
+    }
 }
 
-export { Game, NextAction, ActionResultStatus }
+export { Game }
