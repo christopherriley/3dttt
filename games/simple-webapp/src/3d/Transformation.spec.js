79b1489b7ac9a533dd9babd239fc183fa60097b8
Chris Riley <rileyc@gmail.com>
2020-08-17 00:37:19 -0700

    refactor test

--- games/simple-webapp/src/3d/Transformation.spec.js
+++ games/simple-webapp/src/3d/Transformation.spec.js
@@ -1,31 +1,54 @@
 import { Transformation } from "./Transformation.js"
 
-describe('default transformation', () => {
-    var transformation = new Transformation(0.0, 0.0, 0.0, 1.0, 0.0);
-    var result = [];
-    transformation.calculateTransformationMatrix(result);
+describe('calculateTransformationMatrix test', () => {
+    var result;
+    var transformation;
+    var tx, ty, tz;
+    var scaleFactor;
+    var yRotationInDegrees;
+    var expectedMatrix;
 
-    it("returns expected default matrix", () => {
-        expect(result).toEqual([
+    beforeEach( () => {
+        transformation = new Transformation(tx, ty, tz, scaleFactor, yRotationInDegrees);
+        result = [];
+        transformation.calculateTransformationMatrix(result);
+    });
+
+    describe('default transformation', () => {
+        tx = 0.0;
+        ty = 0.0;
+        tz = 0.0;
+        scaleFactor = 1.0;
+        yRotationInDegrees = 0.0;
+
+        expectedMatrix = [
             1.0, 0.0, -0.0, 0.0,
             0.0, 1.0, 0.0, 0.0,
             0.0, 0.0, 1.0, 0.0,
             0.0, 0.0, 0.0, 1.0
-        ]);
+        ];
+
+        it("returns expected matrix", () => {
+            expect(result).toEqual(expectedMatrix);
+        });
     });
-});
 
-describe('non-default transformation', () => {
-    var transformation = new Transformation(2.0, 3.0, 4.0, 5.0, 6.0);
-    var result = [];
-    transformation.calculateTransformationMatrix(result);
+    describe('non-default transformation', () => {
+        tx = 2.0;
+        ty = 3.0;
+        tz = 4.0;
+        scaleFactor = 5.0;
+        yRotationInDegrees = 6.0;
 
-    it("returns expected default matrix", () => {
-        expect(result).toEqual([
+        expectedMatrix = [
             4.972609476841367, 0.0, -0.5226423163382674, 0.0,
             0.0, 5.0, 0.0, 0.0,
             0.5226423163382674, 0.0, 4.972609476841367, 0.0,
             12.035788219035803, 15.0, 18.845153274688933, 1.0
-        ]);
+        ];
+
+        it("returns expected matrix", () => {
+            expect(result).toEqual(expectedMatrix);
+        });
     });
 });
